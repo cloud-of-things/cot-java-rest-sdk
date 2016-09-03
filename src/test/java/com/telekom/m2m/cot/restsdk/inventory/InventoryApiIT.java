b34e091c9bc06b599b327bda6487660a2debfcab
Patrick Breucking <patrick.breucking@t-systems.com>
2016-09-04 01:09:09 +0200

    Added Integration Test

--- src/test/java/com/telekom/m2m/cot/restsdk/inventory/InventoryApiIT.java
+++ src/test/java/com/telekom/m2m/cot/restsdk/inventory/InventoryApiIT.java
@@ -5,7 +5,7 @@ import com.telekom.m2m.cot.restsdk.util.TestHelper;
 import org.junit.Assert;
 import org.testng.annotations.Test;
 
-import static org.mockito.Matchers.any;
+import java.util.Iterator;
 
 /**
  * Created by breucking on 30.01.16.
@@ -22,6 +22,19 @@ public class InventoryApiIT {
 
         ManagedObject createdMo = inventoryApi.create(mo);
         Assert.assertNotNull("Should now have an Id", mo.getId());
+    }
+
+    @Test
+    public void testGetDevice() throws Exception {
+        CloudOfThingsPlatform cotPlat = new CloudOfThingsPlatform(TestHelper.TEST_TENANT, TestHelper.TEST_USERNAME, TestHelper.TEST_PASSWORD);
+        InventoryApi inventoryApi = cotPlat.getInventoryApi();
+
+        ManagedObject mo = inventoryApi.get("142300");
+        Iterable<ManagedObjectReference> children = mo.getChildDevices().get(5);
+        Iterator<ManagedObjectReference> iter = children.iterator();
+        Assert.assertTrue(iter.hasNext());
+        ManagedObject child = iter.next().getManagedObject();
+        Assert.assertEquals(child.getName(), "RaspPi 8fef9ec2 Sensor BMP180");
     }
 
     @Test
