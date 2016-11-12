1053324cfb02ee82f21458a20e941ee06c6ff7b8
marquies <me@patrickbreucking.de>
2016-11-12 23:31:23 +0100

    Updated API comments in DeviceControlAPI

--- src/main/java/com/telekom/m2m/cot/restsdk/devicecontrol/DeviceControlApi.java
+++ src/main/java/com/telekom/m2m/cot/restsdk/devicecontrol/DeviceControlApi.java
@@ -10,6 +10,9 @@ import com.telekom.m2m.cot.restsdk.util.GsonUtils;
 import java.io.IOException;
 
 /**
+ * DeviceControl API is used to work with operations.
+ *
+ * @since 0.1.0
  * Created by breucking on 31.01.16.
  */
 public class DeviceControlApi {
@@ -18,15 +21,40 @@ public class DeviceControlApi {
     private static final String CONTENT_TYPE = "application/vnd.com.nsn.cumulocity.newDeviceRequest+json;charset=UTF-8;ver=0.9";
     private static final String CONTENT_TYPE_OPERATION = "application/vnd.com.nsn.cumulocity.operation+json;charset=UTF-8;ver=0.9";
 
+    /**
+     * Internal used constructor.
+     * <p>
+     * Use {@link com.telekom.m2m.cot.restsdk.CloudOfThingsPlatform}.getDeviceControlApi()
+     * to create an instance.
+     * </p>
+     *
+     * @param cloudOfThingsRestClient the configured rest client.
+     * @since 0.1.0
+     */
     public DeviceControlApi(CloudOfThingsRestClient cloudOfThingsRestClient) {
         this.cloudOfThingsRestClient = cloudOfThingsRestClient;
     }
 
+    /**
+     * Creates a new Device Request to register new devices.
+     *
+     * @param operation {@link Operation} just with the deviceId to register.
+     * @return the created operation.
+     * @throws IOException if request went wrong.
+     * @since 0.1.0
+     */
     public Operation createNewDevice(Operation operation) throws IOException {
         cloudOfThingsRestClient.doPostRequest(gson.toJson(operation), "devicecontrol/newDeviceRequests", CONTENT_TYPE);
         return operation;
     }
 
+    /**
+     * Accepts a device after it requested the credentials for the first time.
+     *
+     * @param deviceId the Id of the device to accept.
+     * @throws IOException if request went wrong.
+     * @since 0.1.0
+     */
     public void acceptDevice(String deviceId) throws IOException {
         Operation operation = new Operation();
         operation.setStatus(OperationStatus.ACCEPTED);
@@ -34,12 +62,26 @@ public class DeviceControlApi {
         cloudOfThingsRestClient.doPutRequest(gson.toJson(operation), "devicecontrol/newDeviceRequests/" + deviceId, CONTENT_TYPE);
     }
 
+    /**
+     * Retrieves a certain operation.
+     *
+     * @param operationId Id of the operation to retrieve.
+     * @return the Operation as Object.
+     * @since 0.1.0
+     */
     public Operation getOperation(String operationId) {
         String response = cloudOfThingsRestClient.getResponse(operationId, "devicecontrol/operations/", CONTENT_TYPE_OPERATION);
         Operation operation = new Operation(gson.fromJson(response, ExtensibleObject.class));
         return operation;
     }
 
+    /**
+     * Creates an operation.
+     *
+     * @param operation Operation object with the necessary data (w/o Id).
+     * @return the Operation with the created Id.
+     * @since 0.1.0
+     */
     public Operation create(Operation operation) {
         String json = gson.toJson(operation);
 
@@ -49,6 +91,14 @@ public class DeviceControlApi {
         return operation;
     }
 
+    /**
+     * Updates an existing operation state.
+     *
+     * @param operation the operation with new operation state,
+     *                  Id as existing, not changeable.
+     * @return the Operation object.
+     * @since 0.1.0
+     */
     public Operation update(Operation operation) {
         String json = "{\"status\" : \"" + operation.getStatus() + "\"}";
 
