961630e2fd67e55b7fc94ef25ffea734f3604355
coldestbeerintown <ozanarslan85@gmail.com>
2017-08-14 01:41:56 +0200

    Added a minor documentation.

--- src/main/java/com/telekom/m2m/cot/restsdk/users/UserApi.java
+++ src/main/java/com/telekom/m2m/cot/restsdk/users/UserApi.java
@@ -15,6 +15,12 @@ public class UserApi {
 
     private final Gson gson = GsonUtils.createGson();
 
+    /**
+     * Internal Constructor.
+     *
+     * @param cloudOfThingsRestClient
+     *            the configured rest client.
+     */
     public UserApi(CloudOfThingsRestClient cloudOfThingsRestClient) {
         this.cloudOfThingsRestClient = cloudOfThingsRestClient;
     }
