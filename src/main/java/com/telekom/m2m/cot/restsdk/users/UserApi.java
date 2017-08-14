52cdc81c354408942bbd10e88ba2c0eccd1a05b1
rob <r.linden@tarent.de>
2017-08-14 18:01:32 +0200

    4345: updateUser now filters the unupdatable fields.

--- src/main/java/com/telekom/m2m/cot/restsdk/users/UserApi.java
+++ src/main/java/com/telekom/m2m/cot/restsdk/users/UserApi.java
@@ -5,6 +5,8 @@ import com.telekom.m2m.cot.restsdk.CloudOfThingsRestClient;
 import com.telekom.m2m.cot.restsdk.util.ExtensibleObject;
 import com.telekom.m2m.cot.restsdk.util.GsonUtils;
 
+import java.util.Map;
+
 /**
  * Use the UserApi to work with users. Created by Ozan Arslan on 13.07.2017
  */
@@ -544,7 +546,16 @@ public class UserApi {
      */
     public void updateUser(User user, String tenant) {
         String CONTENT = "application/vnd.com.nsn.cumulocity.user+json; charset=UTF-8; ver=0.9";
-        String json = gson.toJson(user);
+
+        Map<String, Object> attributes = user.getAttributes();
+        attributes.remove("id");
+        attributes.remove("userName");
+        attributes.remove("password"); // Cannot be changed in this way for regular users!
+
+        ExtensibleObject extensibleObject = new ExtensibleObject();
+        extensibleObject.setAttributes(attributes);
+
+        String json = gson.toJson(extensibleObject);
         cloudOfThingsRestClient.doPutRequest(json, "user/" + tenant + "/users/" + user.getId(), CONTENT);
     }
 
