ba2951ee3bf10d9a9dc75bf2fad1220c030facb6
coldestbeerintown <ozanarslan85@gmail.com>
2017-08-18 12:18:33 +0200

    Added a few more dummy classes.

---
+++ src/main/java/com/telekom/m2m/cot/restsdk/realtime/Notification.java
@@ -0,0 +1,9 @@
+package com.telekom.m2m.cot.restsdk.realtime;
+
+/**
+ * TODO: currently, merely a placeholder.
+ *
+ */
+public class Notification {
+
+}
---
+++ src/main/java/com/telekom/m2m/cot/restsdk/realtime/SubscriptionListener.java
@@ -0,0 +1,10 @@
+package com.telekom.m2m.cot.restsdk.realtime;
+
+/**
+ * TODO: Currently a placeholder.
+ * A SubscriptionListener is able to receive notifications from the channels that it is subsrcibed to.
+ *
+ */
+public interface SubscriptionListener {
+
+}
