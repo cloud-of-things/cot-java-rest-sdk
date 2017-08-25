dd1b22891eb6211226655eddca8b1cc98cee44f2
coldestbeerintown <ozanarslan85@gmail.com>
2017-08-25 14:27:32 +0200

    And here are the IT tests.

---
+++ src/test/java/com/telekom/m2m/cot/restsdk/realtime/CepApiIT.java
@@ -0,0 +1,248 @@
+package com.telekom.m2m.cot.restsdk.realtime;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.telekom.m2m.cot.restsdk.CloudOfThingsPlatform;
+import com.telekom.m2m.cot.restsdk.alarm.Alarm;
+import com.telekom.m2m.cot.restsdk.alarm.AlarmApi;
+import com.telekom.m2m.cot.restsdk.inventory.ManagedObject;
+import com.telekom.m2m.cot.restsdk.realtime.CepApi;
+import com.telekom.m2m.cot.restsdk.realtime.CepConnector;
+import com.telekom.m2m.cot.restsdk.realtime.Notification;
+import com.telekom.m2m.cot.restsdk.realtime.SubscriptionListener;
+import com.telekom.m2m.cot.restsdk.util.TestHelper;
+
+public class CepApiIT {
+
+    private CloudOfThingsPlatform cotPlat = new CloudOfThingsPlatform(TestHelper.TEST_HOST, TestHelper.TEST_USERNAME,
+            TestHelper.TEST_PASSWORD);
+
+    private CepApi cepApi = cotPlat.getCepApi();
+    private AlarmApi alarmApi = cotPlat.getAlarmApi();
+    private ManagedObject testManagedObject;
+
+    private ManagedObject alarmSource1;
+    private ManagedObject alarmSource2;
+    private ManagedObject alarmSource3;
+
+    private static final int DELAY_MILLIS = 100;
+
+    @BeforeClass
+    public void setUpClass() {
+        testManagedObject = TestHelper.createRandomManagedObjectInPlatform(cotPlat, "fake_name");
+        alarmSource1 = TestHelper.createRandomManagedObjectInPlatform(cotPlat, "fake_name1");
+        alarmSource2 = TestHelper.createRandomManagedObjectInPlatform(cotPlat, "fake_name2");
+        alarmSource3 = TestHelper.createRandomManagedObjectInPlatform(cotPlat, "fake_name3");
+    }
+
+    @AfterClass
+    public void tearDownClass() {
+        TestHelper.deleteManagedObjectInPlatform(cotPlat, testManagedObject);
+    }
+
+    // This test will create one subscriber, then create two alarms (for the
+    // same device/channel), then check if
+    // the listener got the correct notification for each of them.
+    @Test
+    public void testSimpleRealTimeAlarms() throws InterruptedException {
+
+        CepConnector connector = cepApi.getCepConnector();
+
+        final List<String> notedAlarms = new ArrayList<>();
+
+        
+        // Create the subscriptions and listeners:
+        connector.subscribe("/alarms/" + testManagedObject.getId());
+        connector.addListener(new SubscriptionListener() {
+            @Override
+            public void onNotification(String channel, Notification notification) {
+                notedAlarms.add(notification.getData());
+            }
+
+            @Override
+            public void onError(String channel, Throwable error) {
+                fail("There should have been no errors.");
+            }
+        });
+
+        // Connect, starting background listener:
+        connector.connect();
+
+        Thread.sleep(DELAY_MILLIS);
+        alarmApi.create(makeAlarm("com_telekom_TestType1", Alarm.SEVERITY_MINOR, testManagedObject));
+
+        Thread.sleep(DELAY_MILLIS);
+
+        // We need a different type, otherwise our alarm would be de-duplicated
+        // by the server (just increasing
+        // the count of the first one, but not updating the content):
+
+        alarmApi.create(makeAlarm("com_telekom_TestType2", Alarm.SEVERITY_MINOR, testManagedObject));
+
+        Thread.sleep(DELAY_MILLIS);
+
+        assertEquals(notedAlarms.size(), 2);
+
+        assertTrue(notedAlarms.get(0).contains(testManagedObject.getId()));
+        assertTrue(notedAlarms.get(1).contains(testManagedObject.getId()));
+
+    }
+
+    // This test switches subscriptions between two alarm sources and checks
+    // that at each time only the
+    // currently subscribed notifications are received.
+    @Test
+    public void testSubUnsub() throws InterruptedException {
+
+        CepConnector connector = cepApi.getCepConnector();
+
+        // Prepare initial subscription:
+        String channel1 = connector.subscribe("/alarms/" + alarmSource1.getId());
+
+        // The asynchronously received alarms will be stored in this list:
+        final List<String> notedAlarms = new ArrayList<>();
+
+        // Prepare initial subscription and listener for the channel of our test
+        // device:
+        // Prepare listener for the channel of our test device:
+        connector.addListener(new SubscriptionListener() {
+            @Override
+            public void onNotification(String channel, Notification notification) {
+                notedAlarms.add(notification.getData());
+            }
+
+            @Override
+            public void onError(String channel, Throwable error) {
+                fail("There should have been no errors.");
+            }
+        });
+
+        // This alarm, created before we connect & subscribe, should never cause
+        // a notification for us, even
+        // though it has a source to which we will subscribe and receive
+        // notifications later.
+        // The queueing starts with the subscription and continues over
+        // individual connect-response-cycles.
+        // That's why it is possible to reconnect without losing data in the
+        // meantime.
+        // (It cann't continue over new handshakes though (i.e. new clientId))
+        alarmApi.create(makeAlarm("com_telekom_TestType0", Alarm.SEVERITY_MINOR, alarmSource1));
+
+        // Connect, starting background listener:
+        connector.connect();
+
+        Thread.sleep(DELAY_MILLIS);
+
+        alarmApi.create(makeAlarm("com_telekom_TestType1", Alarm.SEVERITY_MINOR, alarmSource1));
+        alarmApi.create(makeAlarm("com_telekom_TestType2", Alarm.SEVERITY_MINOR, alarmSource2));
+
+        Thread.sleep(DELAY_MILLIS);
+
+        // Initially we were subscribed to alarmSource1 only.
+        assertEquals(notedAlarms.size(), 1);
+        assertTrue(notedAlarms.get(0).contains("com_telekom_TestType1"));
+
+        // Now we reverse the subscriptions, from alarmSource1 to alarmSource2,
+        // and we do it twice, to verify that
+        // unnecessary sub-/unsub-requests don't cause problems:
+        connector.unsubscribe(channel1);
+        connector.unsubscribe(channel1);
+        String channel2 = connector.subscribe("/alarms/" + alarmSource2.getId());
+        connector.subscribe("/alarms/" + alarmSource2.getId());
+
+        Thread.sleep(DELAY_MILLIS);
+
+        // At this time we should be subscribed to alarmSource2 only:
+        alarmApi.create(makeAlarm("com_telekom_TestType3", Alarm.SEVERITY_MINOR, alarmSource1));
+        alarmApi.create(makeAlarm("com_telekom_TestType4", Alarm.SEVERITY_MINOR, alarmSource2));
+
+        Thread.sleep(DELAY_MILLIS);
+
+        assertEquals(notedAlarms.size(), 2);
+        assertTrue(notedAlarms.get(1).contains("com_telekom_TestType4"));
+
+        // Now we switch the subscriptions again, back to alarmSource1:
+        connector.unsubscribe(channel2);
+        connector.subscribe("/alarms/" + alarmSource1.getId());
+
+        // At this time we should be subscribed to alarmSource1 again, but the
+        // alarm com_telekom_TestType3, that was
+        // created while we were _not_ subscribed to it's source, will be lost.
+        alarmApi.create(makeAlarm("com_telekom_TestType5", Alarm.SEVERITY_MINOR, alarmSource1));
+        alarmApi.create(makeAlarm("com_telekom_TestType6", Alarm.SEVERITY_MINOR, alarmSource2));
+
+        Thread.sleep(DELAY_MILLIS);
+
+        assertEquals(notedAlarms.size(), 3);
+        assertTrue(notedAlarms.get(2).contains("com_telekom_TestType5"));
+    }
+
+    @Test
+    public void testMultiChannels() throws InterruptedException {
+
+        CepConnector connector = cepApi.getCepConnector();
+
+        // Prepare subscriptions for sources 1 and 2, but not for 3:
+        connector.subscribe("/alarms/" + alarmSource1.getId());
+        connector.subscribe("/alarms/" + alarmSource2.getId());
+
+        // The asynchronously received alarms will be stored in this list:
+        final List<String> notedAlarms = new ArrayList<>();
+
+        // Prepare listener for the channel of our test device:
+        connector.addListener(new SubscriptionListener() {
+            @Override
+            public void onNotification(String channel, Notification notification) {
+                notedAlarms.add(notification.getData());
+            }
+
+            @Override
+            public void onError(String channel, Throwable error) {
+                fail("There should have been no errors.");
+            }
+        });
+
+        // Connect, starting background listener:
+        connector.connect();
+
+        Thread.sleep(DELAY_MILLIS);
+
+        // Create three alarms, but we are subscribed to only the first two.
+        Alarm alarm1 = makeAlarm("com_telekom_TestType1", Alarm.SEVERITY_MINOR, alarmSource1);
+        alarmApi.create(alarm1);
+        Alarm alarm2 = makeAlarm("com_telekom_TestType2", Alarm.SEVERITY_MINOR, alarmSource2);
+        alarmApi.create(alarm2);
+        Alarm alarm3 = makeAlarm("com_telekom_TestType3", Alarm.SEVERITY_MINOR, alarmSource3);
+        alarmApi.create(alarm3);
+
+        Thread.sleep(DELAY_MILLIS);
+
+        assertEquals(notedAlarms.size(), 2);
+        // The data should contain the following fields:
+        assertTrue(notedAlarms.get(0).contains("com_telekom_TestType1"));
+        assertTrue(notedAlarms.get(1).contains("com_telekom_TestType2"));
+    }
+
+    private Alarm makeAlarm(String type, String severity, ManagedObject source) {
+        Alarm alarm = new Alarm();
+        alarm.setText("Strange thing happened!");
+        alarm.setTime(new Date());
+        alarm.setSource(source);
+        alarm.set("foo", "{ \"alt\": " + Math.random() + ", \"lng\": 8.55436, \"lat\": 50.02868 }");
+        alarm.setStatus(Alarm.STATE_ACTIVE);
+        alarm.setType(type);
+        alarm.setSeverity(severity);
+        return alarm;
+    }
+
+}
