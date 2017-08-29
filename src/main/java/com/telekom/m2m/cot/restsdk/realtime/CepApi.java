dd697b1ab2d6a28acd05437ca84563c4dfa6b71f
coldestbeerintown <ozanarslan85@gmail.com>
2017-08-29 16:42:56 +0200

    Further developed the modules. An unnecessary printout is removed from CepConnector.

--- src/main/java/com/telekom/m2m/cot/restsdk/realtime/CepApi.java
+++ src/main/java/com/telekom/m2m/cot/restsdk/realtime/CepApi.java
@@ -1,6 +1,11 @@
 package com.telekom.m2m.cot.restsdk.realtime;
 
+import java.util.Map;
+
+import com.google.gson.Gson;
 import com.telekom.m2m.cot.restsdk.CloudOfThingsRestClient;
+import com.telekom.m2m.cot.restsdk.util.ExtensibleObject;
+import com.telekom.m2m.cot.restsdk.util.GsonUtils;
 
 /**
  * The class that defines the CepApi. CEP stands for Complex-Event-Processing.
@@ -11,21 +16,79 @@ import com.telekom.m2m.cot.restsdk.CloudOfThingsRestClient;
  */
 public class CepApi {
 
-    
     private final CloudOfThingsRestClient cloudOfThingsRestClient;
+    private final Gson gson = GsonUtils.createGson();
 
     public CepApi(CloudOfThingsRestClient cloudOfThingsRestClient) {
         this.cloudOfThingsRestClient = cloudOfThingsRestClient;
     }
-    
-           
+
     /**
-     * Returns the connector that establishes the communications with the notifications service.
+     * Returns the connector that establishes the communications with the
+     * notifications service.
+     * 
      * @return CepConnector
      */
     public CepConnector getCepConnector() {
         return new CepConnector(cloudOfThingsRestClient);
     }
-       
-    
+
+    // TODO: to be tested with the new userName.
+    public Module createModule() {
+        String CONTENT = "multipart/form-data";
+
+        String json = "";
+        String id = cloudOfThingsRestClient.doRequestWithIdResponse(json, "cep/modules", CONTENT);
+
+        Module module = getModule(id);
+        return module;
+
+    }
+
+    // TODO: to be tested with the new userName.
+    public Module getModule(String id) {
+
+        String CONTENT = "application/vnd.com.nsn.cumulocity.cepModule+json;ver=...";
+        String result = cloudOfThingsRestClient.getResponse(id, "cep/modules", CONTENT);
+        return new Module(gson.fromJson(result, ExtensibleObject.class));
+
+    }
+
+    // TODO: to be tested with the new userName.
+    public void updateModule(Module module) {
+
+        String CONTENT = "application/vnd.com.nsn.cumulocity.cepModule+json;ver=...";
+        Map<String, Object> attributes = module.getAttributes();
+        // TODO check which fiels are not allowed to be updated:
+        attributes.remove("exampleFieldToBeRemoved");
+
+        ExtensibleObject extensibleObject = new ExtensibleObject();
+        extensibleObject.setAttributes(attributes);
+
+        String json = gson.toJson(extensibleObject);
+        cloudOfThingsRestClient.doPutRequest(json, "cep/modules/" + module.getId(), CONTENT);
+
+    }
+
+    public void deleteModule(Module module) {
+
+        cloudOfThingsRestClient.delete(module.getId(), "cep/modules");
+
+    }
+
+    // TODO check if we really need a representation of the CepApi such as
+    // below:
+    /*
+     * public CepApiRepresentation getCepApi() {
+     * 
+     * String result = cloudOfThingsRestClient.getResponse("cep", null); return
+     * new CepApiRepresentation(gson.fromJson(result, ExtensibleObject.class));
+     * }
+     * 
+     * public ModuleCollection getModules() { return new
+     * ModuleCollection(cloudOfThingsRestClient, "cep/modules", gson, null);
+     * 
+     * }
+     */
+
 }
