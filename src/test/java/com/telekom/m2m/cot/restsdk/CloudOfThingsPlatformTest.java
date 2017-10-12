e3dd71344bf43ec7908ef58549fe87a5253c1039
Matthias Molitor <matthias@molitor.solutions>
2017-10-12 13:58:52 +0200

    use static test data in CloudOfThingsPlatform unit test to make explicit that the test does *not* require correct credentials

--- src/test/java/com/telekom/m2m/cot/restsdk/CloudOfThingsPlatformTest.java
+++ src/test/java/com/telekom/m2m/cot/restsdk/CloudOfThingsPlatformTest.java
@@ -9,10 +9,13 @@ import org.testng.annotations.Test;
  * Created by Patrick Steinert on 30.01.16.
  */
 public class CloudOfThingsPlatformTest {
+    private final static String TEST_HOST = "https://test.m2m.telekom.com";
+    private final static String TEST_USERNAME = "tester";
+    private final static String TEST_PASSWORD = "anything-goes";
 
     @Test
     public void testGetInventoryApi() throws Exception {
-        CloudOfThingsPlatform cotPlatform = new CloudOfThingsPlatform(TestHelper.TEST_HOST, TestHelper.TEST_USERNAME, TestHelper.TEST_PASSWORD);
+        CloudOfThingsPlatform cotPlatform = new CloudOfThingsPlatform(TEST_HOST, TEST_USERNAME, TEST_PASSWORD);
         InventoryApi inventoryApi = cotPlatform.getInventoryApi();
         Assert.assertNotNull(inventoryApi);
     }
