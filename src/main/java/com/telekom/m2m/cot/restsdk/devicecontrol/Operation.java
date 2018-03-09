0eee8c408ba6b92ee6f0e0c3b1e634f7af18997c
Joshua Gehlen <joshuagehlen@hotmail.com>
2018-03-09 13:34:51 +0100

    Adding failure reason to Operation #51

--- src/main/java/com/telekom/m2m/cot/restsdk/devicecontrol/Operation.java
+++ src/main/java/com/telekom/m2m/cot/restsdk/devicecontrol/Operation.java
@@ -140,4 +140,29 @@ public class Operation extends ExtensibleObject {
         return (String) anyObject.get("deliveryType");
     }
 
+    /**
+     * Setting failure reason in case of failing
+     * @param failureReason a String with failure Reason, e.g. "Operation cancelled by user"
+     */
+    public void setFailureReason(String failureReason){
+        if( failureReason==null){
+            anyObject.remove("failureReason");
+        }else{
+            anyObject.put("failureReason",failureReason);
+        }
+    }
+
+
+    /**
+     * Getting FailureReason, when status is 'FAILED'
+     * @return a String with the failure reason from the Operation
+     */
+    public String getFailureReason(){
+        if(getStatus() == OperationStatus.FAILED) {
+            return (String) anyObject.get("failureReason");
+        }else{
+            return null;
+        }
+    }
 }
+
