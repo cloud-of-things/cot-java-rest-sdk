44b49f50852fde7c602fc3d597eecbff658e1245
Joshua Gehlen <joshuagehlen@hotmail.com>
2018-03-19 16:49:21 +0100

    added getRealtimeAction and getDataPart to notification and added enum realtimeAction

--- src/main/java/com/telekom/m2m/cot/restsdk/realtime/Notification.java
+++ src/main/java/com/telekom/m2m/cot/restsdk/realtime/Notification.java
@@ -1,6 +1,7 @@
 package com.telekom.m2m.cot.restsdk.realtime;
 
 import com.google.gson.JsonObject;
+import com.telekom.m2m.cot.restsdk.util.CotSdkException;
 
 /**
  * The Notification is the object that holds the data/message that a listener receives from the channel that it is subscribed to.
@@ -11,6 +12,24 @@ public  class Notification {
 
     public Notification(JsonObject data) {
         this.data = data;
+    }
+
+    public Enum<RealtimeAction> getRealtimeAction() {
+        try {
+            String realtimeAction = data.get("realtimeAction").getAsString();
+            return RealtimeAction.valueOf(realtimeAction);
+        } catch (NullPointerException e) {
+            throw new CotSdkException("No member realtimeAction in Json Object");
+        }
+
+    }
+
+    public String getDataPart() {
+        try {
+            return data.get("data").getAsString();
+        } catch (NullPointerException e) {
+            throw new CotSdkException("No member data in Json Object");
+        }
     }
 
 
---
+++ src/main/java/com/telekom/m2m/cot/restsdk/realtime/RealtimeAction.java
@@ -0,0 +1,10 @@
+package com.telekom.m2m.cot.restsdk.realtime;
+
+public enum RealtimeAction {
+    /**
+     * there are three different values for a realtime Action 
+     */
+    CREATE,
+    DELETE,
+    UPDATE
+}
