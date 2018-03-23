409857a1dc328652dfa121336d9c8f4bf3ddda7a
Joshua Gehlen <joshuagehlen@hotmail.com>
2018-03-23 13:29:15 +0100

    add enum for FilterBy values

---
+++ src/main/java/com/telekom/m2m/cot/restsdk/util/FilterBy.java
@@ -0,0 +1,40 @@
+package com.telekom.m2m.cot.restsdk.util;
+
+public enum FilterBy {
+    /**
+     * perrmitted filters from all apis
+     */
+    BYSOURCE ("source"),
+    BYSTATUS ("status"),
+    BYAGENTID ("agentId"),
+    BYTYPE ("type"),
+    BYDATEFROM ("dateFrom"),
+    BYDATETO ("dateTo"),
+    BYFRAGMENTTYPE ("fragmentType"),
+    BYDEVICEID ("deviceId"),
+    BYTEXT ("Text"),
+    BYLISTOFIDs ("ids"),
+    BYUSER ("User"),
+    BYAPPLICATION ("application");
+
+    private String filterKey;
+    FilterBy(String filterKey) {
+        this.filterKey = filterKey;
+    }
+
+    public String getFilterKey() {
+        return filterKey;
+    }
+
+    public static FilterBy getFilterBy(String filter) {
+
+      for(FilterBy b : values()) {
+          if(b.getFilterKey().equalsIgnoreCase(filter)) {
+              return b;
+          }
+      }
+      throw new IllegalArgumentException(String.format("Couldn't find an enum for requested filter: [%s]", filter));
+
+    }
+
+}
