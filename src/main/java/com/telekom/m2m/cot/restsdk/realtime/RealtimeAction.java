31655794f9670e9008ba58966935555e127e4d80
Joshua Gehlen <joshuagehlen@hotmail.com>
2018-03-27 12:24:49 +0200

    add doc block to realtime action

--- src/main/java/com/telekom/m2m/cot/restsdk/realtime/RealtimeAction.java
+++ src/main/java/com/telekom/m2m/cot/restsdk/realtime/RealtimeAction.java
@@ -1,5 +1,9 @@
 package com.telekom.m2m.cot.restsdk.realtime;
 
+/**
+ * A realtime action represents a type of notifications.
+ * It is a field in the realtime notification.
+ */
 public enum RealtimeAction {
     /**
      * there are three different values for a realtime Action 
