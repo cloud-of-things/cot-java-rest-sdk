e7a6e7dcc9ba7ba45e96825c10e854e62a81b3ff
Matthias Molitor <matthias@molitor.solutions>
2018-05-02 17:23:14 +0200

    added JSON template; implemented tests

--- src/test/java/com/telekom/m2m/cot/restsdk/util/IterableObjectPaginationTest.java
+++ src/test/java/com/telekom/m2m/cot/restsdk/util/IterableObjectPaginationTest.java
@@ -1,16 +1,22 @@
 package com.telekom.m2m.cot.restsdk.util;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.telekom.m2m.cot.restsdk.CloudOfThingsRestClient;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import javax.annotation.Nonnull;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -42,7 +48,7 @@ public class IterableObjectPaginationTest {
         ) {
             @Nonnull
             @Override
-            protected ExtensibleObject convertJsonToObject(@Nonnull JsonElement element) {
+            protected ExtensibleObject convertJsonToObject(@Nonnull final JsonElement element) {
                 return gson.fromJson(element, ExtensibleObject.class);
             }
         };
@@ -74,6 +80,7 @@ public class IterableObjectPaginationTest {
 
         assertEquals(5, objects.size());
         assertPageNotRequested(3);
+        assertPageNotRequested(4);
     }
 
     @Test
@@ -86,6 +93,7 @@ public class IterableObjectPaginationTest {
         assertPageNotRequested(1);
         assertPageNotRequested(2);
         assertPageNotRequested(3);
+        assertPageNotRequested(4);
     }
 
     @Test
@@ -98,7 +106,34 @@ public class IterableObjectPaginationTest {
     }
 
     private void simulateNumberOfObjectsOnPages(final int numberOfObjects) {
-
+        if (numberOfObjects == 0) {
+            simulatePageResponse(1, readTemplate("pages/no-filter/empty-page.json"));
+            return;
+        }
+        final String pageTemplate = readTemplate("pages/no-filter/page.json.template");
+        final String itemTemplate = readTemplate("pages/no-filter/item.json.template");
+        final int totalPages = ceilDiv(numberOfObjects, PAGE_SIZE_IN_TESTS);
+        for (int page = 1; page <= totalPages; page++) {
+            final int itemsOnPage = (page == totalPages) ? numberOfObjects % PAGE_SIZE_IN_TESTS : PAGE_SIZE_IN_TESTS;
+            final String itemsJson = IntStream.range(1, itemsOnPage)
+                .mapToObj(id -> itemTemplate.replace("%%id%%", String.valueOf(id)))
+                .collect(Collectors.joining(","));
+            final String body = pageTemplate
+                .replace("%%totalPages%%", String.valueOf(totalPages))
+                .replace("%%pageSize%%", String.valueOf(PAGE_SIZE_IN_TESTS))
+                .replace("%%currentPage%%", String.valueOf(page))
+                .replace("%%previousPage%%", String.valueOf(page - 1))
+                .replace("%%nextPage%%", String.valueOf(page + 1))
+                .replace("%%items%%", itemsJson);
+            final JsonObject document = GsonUtils.createGson(true).fromJson(body, JsonElement.class).getAsJsonObject();
+            if (page == 1) {
+                document.remove("prev");
+            }
+            if (page == totalPages) {
+                document.remove("next");
+            }
+            simulatePageResponse(page, document.toString());
+        }
     }
 
     /**
@@ -106,10 +141,50 @@ public class IterableObjectPaginationTest {
      *
      * @param pageNumber The 1-indexed page number.
      */
-    private void assertPageNotRequested(int pageNumber) {
+    private void assertPageNotRequested(final int pageNumber) {
         assertTrue(
             pageNumber > 0,
             "Invalid page number value '" + pageNumber + "', page numbers *must* start at one."
         );
+        verify(cloudOfThingsRestClient, never()).getResponse(eq(pageUrl(pageNumber)));
+    }
+
+    /**
+     * Simulates a HTTP response body for the given page.
+     *
+     * @param pageNumber The number of the simulated page.
+     * @param body The returned content.
+     */
+    private void simulatePageResponse(final int pageNumber, @Nonnull final String body) {
+        doReturn(body)
+            .when(cloudOfThingsRestClient)
+            .getResponse(eq(pageUrl(pageNumber)));
+    }
+
+    /**
+     * @param pageNumber The 1-indexed page number.
+     * @return The API URL to the page.
+     */
+    @Nonnull
+    private String pageUrl(final int pageNumber) {
+        return "test/url?currentPage=" + pageNumber + "&pageSize=" + PAGE_SIZE_IN_TESTS;
+    }
+
+    /**
+     * Reads the content of a test data file.
+     *
+     * @param fileResourcePath Path to the resource, relative the test package.
+     * @return The file content.
+     */
+    @Nonnull
+    private String readTemplate(final String fileResourcePath) {
+        final InputStream input = getClass().getResourceAsStream(fileResourcePath);
+        try (final Scanner scanner = new Scanner(input, StandardCharsets.UTF_8.name())) {
+            return scanner.useDelimiter("\\A").next();
+        }
+    }
+
+    private int ceilDiv(final int dividend, final int divisor){
+        return -Math.floorDiv(-dividend,divisor);
     }
 }
