89ff7178fbf38ecb91eb7c4be26fe5ec5cd98eaf
Ganna Krupii <ganna.krupii.axxessio@gmail.com>
2018-10-10 14:16:15 +0200

    create managed objects in cloud before each test and delete afterwards

--- src/test/java/com/telekom/m2m/cot/restsdk/inventory/InventoryApiCrudIT.java
+++ src/test/java/com/telekom/m2m/cot/restsdk/inventory/InventoryApiCrudIT.java
@@ -5,11 +5,10 @@ import com.google.gson.JsonPrimitive;
 import com.telekom.m2m.cot.restsdk.CloudOfThingsPlatform;
 import com.telekom.m2m.cot.restsdk.util.TestHelper;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 import static org.testng.Assert.*;
 
@@ -21,60 +20,49 @@ public class InventoryApiCrudIT {
     private static final String MANAGED_OBJECT_NAME = "managedObjectName";
     private static final String CHILD_MANAGED_OBJECT_NAME = "RaspPi 8fef9ec2 Sensor BMP180";
 
-    CloudOfThingsPlatform cloudOfThingsPlatform = new CloudOfThingsPlatform(TestHelper.TEST_HOST, TestHelper.TEST_USERNAME, TestHelper.TEST_PASSWORD);
-    InventoryApi inventoryApi = cloudOfThingsPlatform.getInventoryApi();
+    private CloudOfThingsPlatform cloudOfThingsPlatform = new CloudOfThingsPlatform(TestHelper.TEST_HOST, TestHelper.TEST_USERNAME, TestHelper.TEST_PASSWORD);
+    private InventoryApi inventoryApi = cloudOfThingsPlatform.getInventoryApi();
 
-    private List<ManagedObject> managedObjectsToDelete = new ArrayList<>();
+    private ManagedObject managedObject;
+    private ManagedObject childManagedObject;
+
+    @BeforeClass
+    public void setUp() {
+        managedObject = TestHelper.createRandomManagedObjectInPlatform(cloudOfThingsPlatform, MANAGED_OBJECT_NAME);
+        childManagedObject = TestHelper.createRandomManagedObjectInPlatform(cloudOfThingsPlatform, CHILD_MANAGED_OBJECT_NAME);
+    }
 
     @AfterMethod
     public void tearDown() {
-        for (ManagedObject managedObject : managedObjectsToDelete) {
-            inventoryApi.delete(managedObject.getId());
-        }
-        managedObjectsToDelete.clear();
+        TestHelper.deleteManagedObjectInPlatform(cloudOfThingsPlatform, managedObject);
+        TestHelper.deleteManagedObjectInPlatform(cloudOfThingsPlatform, childManagedObject);
     }
 
     @Test
     public void testCreateManagedObject() {
-        ManagedObject mo = new ManagedObject();
-        mo.setName(MANAGED_OBJECT_NAME);
-        ManagedObject createdMo = inventoryApi.create(mo);
-        managedObjectsToDelete.add(createdMo);
-        ManagedObject retrievedMo = inventoryApi.get(createdMo.getId());
+        ManagedObject retrievedMo = inventoryApi.get(managedObject.getId());
 
-        assertNotNull(mo.getId(), "Created managed object should have an ID.");
-        assertEquals(retrievedMo.getId(), createdMo.getId(), "ID of created managed object and managed object retrieved from cloud should be the same.");
+        assertNotNull(managedObject.getId(), "Created managed object should have an ID.");
+        assertEquals(retrievedMo.getId(), managedObject.getId(), "ID of created managed object and managed object retrieved from cloud should be the same.");
         assertEquals(retrievedMo.getName(), MANAGED_OBJECT_NAME, "Name of created managed object and managed object retrieved from cloud should be the same.");
     }
 
     @Test
     public void testDeleteManagedObject() {
-        ManagedObject mo = new ManagedObject();
-        mo.setName(MANAGED_OBJECT_NAME);
-        ManagedObject createdMo = inventoryApi.create(mo);
-        ManagedObject retrievedMo = inventoryApi.get(createdMo.getId());
+        ManagedObject retrievedMo = inventoryApi.get(managedObject.getId());
 
         assertNotNull(retrievedMo.getId(), "Created managed object should have an ID.");
 
         inventoryApi.delete(retrievedMo.getId());
-        retrievedMo = inventoryApi.get(createdMo.getId());
+        retrievedMo = inventoryApi.get(managedObject.getId());
+
         assertNull(retrievedMo, "After deletion managed object should not exist anymore in the cloud.");
     }
 
     @Test
     public void testRegisterAsChildDevice() {
-        ManagedObject mo = new ManagedObject();
-        mo.setName(MANAGED_OBJECT_NAME);
-        ManagedObject moChild = new ManagedObject();
-        moChild.setName(CHILD_MANAGED_OBJECT_NAME);
-
-        ManagedObject createdMo = inventoryApi.create(mo);
-        ManagedObject createdMoChild = inventoryApi.create(moChild);
-        managedObjectsToDelete.add(createdMo);
-        managedObjectsToDelete.add(createdMoChild);
-
-        inventoryApi.registerAsChildDevice(createdMo, createdMoChild);
-        ManagedObject retrievedMo = inventoryApi.get(createdMo.getId());
+        inventoryApi.registerAsChildDevice(managedObject, childManagedObject);
+        ManagedObject retrievedMo = inventoryApi.get(managedObject.getId());
         ManagedObjectReferenceCollection childDevices = retrievedMo.getChildDevices();
 
         assertNotNull(childDevices.getSelf());
@@ -89,11 +77,7 @@ public class InventoryApiCrudIT {
 
     @Test
     public void testCreateReadUpdateDelete() {
-        ManagedObject mo = new ManagedObject();
-        mo.setName(MANAGED_OBJECT_NAME);
-        ManagedObject createdMo = inventoryApi.create(mo);
-        managedObjectsToDelete.add(createdMo);
-        ManagedObject retrievedMo = inventoryApi.get(createdMo.getId());
+        ManagedObject retrievedMo = inventoryApi.get(managedObject.getId());
 
         JsonObject obj = new JsonObject();
         obj.add("foo", new JsonPrimitive("bar"));
@@ -102,9 +86,9 @@ public class InventoryApiCrudIT {
         retrievedMo.set("play", obj);
 
         inventoryApi.update(retrievedMo);
-        ManagedObject updatedMo = inventoryApi.get(createdMo.getId());
+        ManagedObject updatedMo = inventoryApi.get(managedObject.getId());
 
-        assertEquals(updatedMo.getId(), createdMo.getId(), "ID of created and updated managed object should be the same.");
+        assertEquals(updatedMo.getId(), managedObject.getId(), "ID of created and updated managed object should be the same.");
         assertEquals(updatedMo.getName(), "NewName", "Name of the managed object should have been updated in the cloud.");
 
         Object retrievedObject = retrievedMo.get("play");
