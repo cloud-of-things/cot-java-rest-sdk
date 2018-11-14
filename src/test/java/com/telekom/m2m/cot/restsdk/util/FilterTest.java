e1ce2aa15240333f652d9244d90273dad74ceaf2
Matthias Molitor <matthias@molitor.solutions>
2018-11-14 19:36:55 +0100

    added test case stub for Filter

---
+++ src/test/java/com/telekom/m2m/cot/restsdk/util/FilterTest.java
@@ -0,0 +1,96 @@
+package com.telekom.m2m.cot.restsdk.util;
+
+import org.junit.Test;
+
+public class FilterTest {
+
+    @Test
+    public void bySourceAddsValueToFilterString() {
+
+    }
+
+    @Test
+    public void byTypeAddsValueToFilterString() {
+
+    }
+
+    @Test
+    public void byDateAddsValuesToFilterString() {
+
+    }
+
+    @Test
+    public void byFragmentTypeAddsValueToFilterString() {
+
+    }
+
+    @Test
+    public void byDeviceIdAddsValueToFilterString() {
+
+    }
+
+    @Test
+    public void byStatusAddsValueToFilterString() {
+
+    }
+
+    @Test
+    public void byStatusWithOperationParameterAddsValueToFilterString() {
+
+    }
+
+    @Test
+    public void byTextAddsValueToFilterString() {
+
+    }
+
+    @Test
+    public void byListOfIdsAddsValueToFilterString() {
+
+    }
+
+    @Test
+    public void byAgentIdAddsValueToFilterString() {
+
+    }
+
+    @Test
+    public void byUserAddsValueToFilterString() {
+
+    }
+
+    @Test
+    public void byApplicationAddsValueToFilterString() {
+
+    }
+
+    @Test
+    public void multipleFiltersAreCombined() {
+
+    }
+
+    @Test
+    public void setFilterAddsProvidedFilter() {
+
+    }
+
+    @Test
+    public void setFiltersAddsAllProvidedFilters() {
+
+    }
+
+    @Test
+    public void validateSupportedFiltersDoesNotThrowExceptionIfNullIsPassed() {
+
+    }
+
+    @Test
+    public void validateSupportedFiltersDoesNotThrowExceptionIfOnlyAcceptedFiltersHaveBeenAdded() {
+
+    }
+
+    @Test
+    public void validateSupportedFiltersThrowsExceptionIfNotAcceptedFilterHasBeenAdded() {
+
+    }
+}
