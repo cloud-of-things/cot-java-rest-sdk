b22a3ce3e924433c4793f1447227b85358558443
Matthias Molitor <matthias@molitor.solutions>
2018-11-15 17:36:47 +0100

    documented non-nullable parameters/return values

--- src/main/java/com/telekom/m2m/cot/restsdk/util/FilterBy.java
+++ src/main/java/com/telekom/m2m/cot/restsdk/util/FilterBy.java
@@ -1,5 +1,7 @@
 package com.telekom.m2m.cot.restsdk.util;
 
+import javax.annotation.Nonnull;
+
 public enum FilterBy {
     /**
      * permitted filters from all apis
@@ -17,17 +19,20 @@ public enum FilterBy {
     BYUSER ("user"),
     BYAPPLICATION ("application");
 
+    @Nonnull
     private String filterKey;
 
-    FilterBy(String filterKey) {
+    FilterBy(@Nonnull final String filterKey) {
         this.filterKey = filterKey;
     }
 
+    @Nonnull
     public String getFilterKey() {
         return filterKey;
     }
 
-    public static FilterBy getFilterBy(String filter) {
+    @Nonnull
+    public static FilterBy getFilterBy(@Nonnull final String filter) {
 
       for(FilterBy b : values()) {
           if(b.getFilterKey().equalsIgnoreCase(filter)) {
@@ -35,7 +40,6 @@ public enum FilterBy {
           }
       }
       throw new IllegalArgumentException(String.format("Couldn't find an enum for requested filter: [%s]", filter));
-
     }
 
 }
