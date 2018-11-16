7a8826e19dd36e5374682f9df7b66313a4886e8b
Matthias Molitor <matthias@molitor.solutions>
2018-11-16 11:32:31 +0100

    attribute can be private

--- src/test/java/com/telekom/m2m/cot/restsdk/alarm/AlarmApiIT.java
+++ src/test/java/com/telekom/m2m/cot/restsdk/alarm/AlarmApiIT.java
@@ -15,7 +15,7 @@ import java.util.Date;
  */
 public class AlarmApiIT {
 
-    CloudOfThingsPlatform cotPlat = new CloudOfThingsPlatform(TestHelper.TEST_HOST, TestHelper.TEST_USERNAME, TestHelper.TEST_PASSWORD);
+    private CloudOfThingsPlatform cotPlat = new CloudOfThingsPlatform(TestHelper.TEST_HOST, TestHelper.TEST_USERNAME, TestHelper.TEST_PASSWORD);
     private ManagedObject testManagedObject;
 
     @BeforeClass
