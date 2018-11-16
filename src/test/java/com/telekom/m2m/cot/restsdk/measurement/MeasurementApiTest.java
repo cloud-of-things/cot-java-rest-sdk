09a70805b53d6df00a7f24fd9df5e137ef0b366d
Matthias Molitor <matthias@molitor.solutions>
2018-11-16 13:03:51 +0100

    extracted setup code

--- src/test/java/com/telekom/m2m/cot/restsdk/measurement/MeasurementApiTest.java
+++ src/test/java/com/telekom/m2m/cot/restsdk/measurement/MeasurementApiTest.java
@@ -2,22 +2,43 @@ package com.telekom.m2m.cot.restsdk.measurement;
 
 import com.telekom.m2m.cot.restsdk.CloudOfThingsRestClient;
 import com.telekom.m2m.cot.restsdk.util.CotSdkException;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mockito;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by Patrick Steinert on 07.02.16.
  */
 public class MeasurementApiTest {
 
+    /**
+     * System under test.
+     */
+    private MeasurementApi measurementApi;
+
+    /**
+     * Mocked client that is used by the test subject.
+     */
+    private CloudOfThingsRestClient restClient;
+
+    @BeforeMethod
+    public void setup() {
+        restClient = mock(CloudOfThingsRestClient.class);
+        measurementApi = new MeasurementApi(restClient);
+    }
+
     @Test(expectedExceptions = CotSdkException.class)
     public void testGetEventWithFailure() {
-        CloudOfThingsRestClient rc = Mockito.mock(CloudOfThingsRestClient.class);
-        Mockito.doThrow(CotSdkException.class).when(rc).getResponse(any(String.class), any(String.class), any(String.class));
+        doThrow(CotSdkException.class)
+            .when(restClient)
+            .getResponse(any(String.class), any(String.class), any(String.class));
 
-        MeasurementApi measurementApi = new MeasurementApi(rc);
         measurementApi.getMeasurement("1234");
     }
 
