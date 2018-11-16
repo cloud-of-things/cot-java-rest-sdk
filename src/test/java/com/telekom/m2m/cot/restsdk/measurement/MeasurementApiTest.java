3cef7fcb69516d651be6fceea79457aab814054e
Matthias Molitor <matthias@molitor.solutions>
2018-11-16 13:07:46 +0100

    added test that reveals a nullpointer problem

--- src/test/java/com/telekom/m2m/cot/restsdk/measurement/MeasurementApiTest.java
+++ src/test/java/com/telekom/m2m/cot/restsdk/measurement/MeasurementApiTest.java
@@ -2,15 +2,12 @@ package com.telekom.m2m.cot.restsdk.measurement;
 
 import com.telekom.m2m.cot.restsdk.CloudOfThingsRestClient;
 import com.telekom.m2m.cot.restsdk.util.CotSdkException;
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
-import org.mockito.Mockito;
+import com.telekom.m2m.cot.restsdk.util.Filter;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Patrick Steinert on 07.02.16.
@@ -42,4 +39,22 @@ public class MeasurementApiTest {
         measurementApi.getMeasurement("1234");
     }
 
+    @Test(expectedExceptions = CotSdkException.class)
+    public void testThrowsExceptionIfClientReturnsNull() {
+        doReturn(null)
+            .when(restClient)
+            .getResponse(any(String.class), any(String.class), any(String.class));
+
+        measurementApi.getMeasurement("1234");
+    }
+
+    @Test
+    public void deleteMeasurementsDoesNotThrowExceptionIfNoFilterIsPassed() {
+        measurementApi.deleteMeasurements(null);
+    }
+
+    @Test
+    public void deleteMeasurementsDoesNotThrowExceptionIfFiltersAreProvided() {
+        measurementApi.deleteMeasurements(Filter.build().bySource("test"));
+    }
 }
