f0935001a4b0a5c1534f8ee9732b360831ad5a5d
Matthias Molitor <matthias@molitor.solutions>
2018-11-16 11:51:30 +0100

    refactoring: extracted method

--- src/test/java/com/telekom/m2m/cot/restsdk/alarm/AlarmApiTest.java
+++ src/test/java/com/telekom/m2m/cot/restsdk/alarm/AlarmApiTest.java
@@ -6,6 +6,7 @@ import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import javax.annotation.Nonnull;
 import java.util.Date;
 
 import static org.mockito.Matchers.any;
@@ -15,28 +16,27 @@ import static org.mockito.Matchers.any;
  */
 public class AlarmApiTest {
 
+    private static final String ALARM_JSON_EXAMPLE = "{\n" +
+        "  \"id\" : \"10\",\n" +
+        "  \"self\" : \"<<Alarm URL>>\",\n" +
+        "  \"creationTime\" : \"2011-09-06T12:03:27.927Z\",\n" +
+        "  \"type\" : \"com_telekom_events_TamperEvent\",\n" +
+        "  \"time\" : \"2011-09-06T12:03:27.845Z\",\n" +
+        "  \"text\" : \"Tamper sensor triggered\",\n" +
+        "  \"status\" : \"ACTIVE\",\n" +
+        "  \"severity\" : \"MAJOR\",\n" +
+        "  \"source\" : { \"id\" : \"12345\", \"self\" : \"...\" },\n" +
+        "  \"com_mycorp_MyProp\" : {  },\n" +
+        "  \"history\" : {\n" +
+        "    \"self\" : \"...\",\n" +
+        "    \"auditRecords\" : [ ]\n" +
+        "  }\n" +
+        "}";
+
     @Test
     public void testGetAlarm() {
 
-        String alarmJsonExample = "{\n" +
-                "  \"id\" : \"10\",\n" +
-                "  \"self\" : \"<<Alarm URL>>\",\n" +
-                "  \"creationTime\" : \"2011-09-06T12:03:27.927Z\",\n" +
-                "  \"type\" : \"com_telekom_events_TamperEvent\",\n" +
-                "  \"time\" : \"2011-09-06T12:03:27.845Z\",\n" +
-                "  \"text\" : \"Tamper sensor triggered\",\n" +
-                "  \"status\" : \"ACTIVE\",\n" +
-                "  \"severity\" : \"MAJOR\",\n" +
-                "  \"source\" : { \"id\" : \"12345\", \"self\" : \"...\" },\n" +
-                "  \"com_mycorp_MyProp\" : {  },\n" +
-                "  \"history\" : {\n" +
-                "    \"self\" : \"...\",\n" +
-                "    \"auditRecords\" : [ ]\n" +
-                "  }\n" +
-                "}";
-
-        CloudOfThingsRestClient client = Mockito.mock(CloudOfThingsRestClient.class);
-        Mockito.when(client.getResponse(any(String.class), any(String.class), any(String.class))).thenReturn(alarmJsonExample);
+        CloudOfThingsRestClient client = createClient();
 
 
         AlarmApi alarmApi = new AlarmApi(client);
@@ -49,5 +49,15 @@ public class AlarmApiTest {
         Assert.assertEquals(alarm.getTime().compareTo(new Date(1315310607845L)), 0);
 
         Assert.assertNotNull(alarm.get("com_mycorp_MyProp"));
+    }
+
+    /**
+     * @return A mocked Cloud of Things REST client.
+     */
+    @Nonnull
+    private CloudOfThingsRestClient createClient() {
+        CloudOfThingsRestClient client = Mockito.mock(CloudOfThingsRestClient.class);
+        Mockito.when(client.getResponse(any(String.class), any(String.class), any(String.class))).thenReturn(ALARM_JSON_EXAMPLE);
+        return client;
     }
 }
